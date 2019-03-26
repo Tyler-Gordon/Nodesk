af378ab3a62e3b0046b9b5062246451b52911d59
ConnorVin <43933461+ConnorVin@users.noreply.github.com>
2019-03-26 08:52:52 -0700

    Updated server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const server = require('http').createServer();
 const fs = require('fs');
 const qs = require('querystring');
+const registerUser = require('./userRegistration').registerUser;
 
 server.on('request', (req, res) => {
     switch (req.url) {
@@ -10,28 +11,25 @@ server.on('request', (req, res) => {
             res.end(fs.readFileSync(`./loginpage.html`));
             break;
 
-        // case '/':
-        //     if (req.method === 'POST') {
-        //         var body = '';
+        case '/submit':
+            if (req.method === 'POST') {
+                var body = '';
 
-        //         req.on('data', data => {
-        //             body += data;
-        //         })
+                req.on('data', data => {
+                    body += data;
+                })
 
-        //         req.on('end', () => {
-        //             console.log(qs.parse(body));
-        //         })
-        //     }
+                req.on('end', () => {
+                    body = qs.parse(body)
+                    registerUser(body.username, body.email, body.passwordCheck)
+                })
+            }
 
-        //     res.writeHead(200, { 'Content-Type': 'text/html' });
-        //     res.end(fs.readFileSync(`.${req.url}.html`));
-        //     break;
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Thank you for registering');
+            break;
 
         default:
-            req.on('data', data => {
-                console.log(data)
-            });
-            
             res.writeHead(404);
             res.end();
     }
