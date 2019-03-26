299ccd09845960b0c90f047f29289ab709d3ea27
Tylertytywow <tylerjg1042@gmail.com>
2019-03-25 20:24:30 -0700

    Actually added the uniqueness
    Forgot to add it!

--- userRegistration.js
+++ userRegistration.js
@@ -1,18 +1,28 @@
-const mongoClient = require('mongodb').MongoClient,
+const mongoClient = require('mongodb').MongoClient
 const crypto = require('crypto')
 const dataBase = 'learning-node';
 var dataBaseURL = 'mongodb://tyler:node2520@ds125073.mlab.com:25073/learning-node'
 
 var registerUser = async(username, email, password) =>{
     encryptedPassword = crypto.createHmac('sha256',password).update('password').digest('hex');
-
     var userinfo = {username:username,email:email,password:encryptedPassword}
-    
-    mongoClient.connect(dataBaseURL, (err,client) => {
-        console.log('DB CONNECTED')
+
+    mongoClient.connect(dataBaseURL,{useNewUrlParser:true},async (err,client) => {
         const database = client.db(dataBase);
-        database.collection('users').insert(userinfo);
-        client.close();
+        var collection = database.collection('users')
+        var userExists = (await collection.find({username:username}).toArray() <= 0)
+        
+        if(userExists){
+            collection.insertOne(userinfo);
+            console.log('USER PUBLISHED')
+            client.close();
+            return 301
+        }
+        else{ 
+            console.log('USER EXISTS')
+            client.close();
+            return 400
+        }
     })
 }
 module.exports = {registerUser};
