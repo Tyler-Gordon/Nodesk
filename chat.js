6f0e4886bdaed3c59eb2f90f150a00748b47d99f
Tylertytywow <tylerjg1042@gmail.com>
2019-03-30 23:31:10 -0700

    Added some comments

--- chat.js
+++ chat.js
@@ -3,12 +3,15 @@ const crypto = require('crypto')
 const dataBase = 'learning-node';
 var dataBaseURL = 'mongodb://tyler:node2520@ds125073.mlab.com:25073/learning-node'
  
-
+// Models, and inserts a new chat into the database
 var createChat = (users) =>{
     mongoClient.connect(dataBaseURL, { useNewUrlParser:true }, (err, client) => {
+        // access the database
         const database = client.db(dataBase);
+        // get the 'chats' table
         var chatsCollection = database.collection('chats')
         var chatId = crypto.randomBytes(10).toString('hex');
+        // create a model for the chat, this will allow easy, predictable database entries
         var chatModel = {'_id':chatId,messages : [],users : users}
         chatsCollection.insertOne(chatModel)
     })
--- messages.js
+++ messages.js
@@ -3,27 +3,32 @@ const crypto = require('crypto')
 const dataBase = 'learning-node';
 var dataBaseURL = 'mongodb://tyler:node2520@ds125073.mlab.com:25073/learning-node'
 
+// Adds a message to the chat, given the chatid passed through messageObject
 var addMessage = (messageObject) =>{
     mongoClient.connect(dataBaseURL, { useNewUrlParser:true }, (err, client) => {
+        // get the database
         const database = client.db(dataBase);
+        // get the 'chats' table
         var collection = database.collection('chats')
-        collection.update(
-            {_id:messageObject.chatId},
-            {$push:
-                {messages:messageObject}
-            }
+        // push the new message into the chat based on its id
+        collection.update({_id:messageObject.chatId},{$push:{messages:messageObject}}
         )
     })
 }
 
+// Grabs all messages from the given chatid
 var getMessages = (chatId, callback) =>{
     mongoClient.connect(dataBaseURL, { useNewUrlParser:true }, (err, client) => {
+        // get the database
         const database = client.db(dataBase);
+        // get the 'chats' table
         var collection = database.collection('chats')
+        // find the messages based on the chat id
         collection.findOne(({'_id':chatId}),(err,data)=>{
             return('all messages')
         })
     })
+    
     callback(listOfChatObjects);
 }
 module.exports = {addMessage,getMessages};
