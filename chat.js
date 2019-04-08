582da1ebaef927c303092cf180e8a2aab2451473
Tyler-Gordon <tylerjg1042@gmail.com>
2019-04-08 14:37:13 -0700

    Stuff

--- chat.js
+++ chat.js
@@ -10,21 +10,33 @@ var createChat = (users,callback) =>{
         const database = client.db(dataBase);
         // get the 'chats' table
         var chatsCollection = database.collection('chats')
+        var userCollection = database.collection('users')
         var chatId = crypto.randomBytes(10).toString('hex');
         // create a model for the chat, this will allow easy, predictable database entries
         var chatModel = {'_id':chatId,messages : [],users : users}
 
-        dbUsernames = userCollection.distinct('username')
-        users.forEach(user => {
-            if(!dbUsernames.includes(user)){
-
-                var e = Error(`User ${user} does not exist`)
-                e.name = 'UserError'
-                throw e
-            }
-        });
-        chatsCollection.insertOne(chatModel)
-        callback()
+        // userCollection.distinct("username", (dbUsernames) => {
+            // console.log(dbUsernames);
+            users.forEach(user => {
+                userCollection.findOne(({"username" : user}), (err, data) => {
+                    console.log(data.username)
+                    if(data.username !== user){
+                        var e = Error(`User ${user} does not exist`)
+                        e.name = 'UserError'
+                        throw e
+                    }
+                    
+                })
+            });
+        // })
+        chatsCollection.insertOne(chatModel,(err,result)=>{
+            users.forEach(user=>{
+                userCollection.update({username:user},{$push:{chatids:result.insertedId}}
+                )
+            })
+            callback(result.insertedId)
+        })
+        
     })
 }
 
@@ -49,7 +61,7 @@ var getChatIDs = (username,callback) => {
         var userCollection = database.collection('users')
         // create a model for the chat, this will allow easy, predictable database entries
         userCollection.findOne(({"username":username}),(err,data)=>{    
-            callback(data.chatids);
+            callback(data.users);
         })
     })
 }
